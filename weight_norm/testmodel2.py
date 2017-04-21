bb2abe7c93ce6a4c9e336e788ed036e439141ce1
btran115 <btran115@mit.edu>
2017-04-21 14:25:00 -0400

    added models that work

--- weight_norm/testmodel2.py
+++ weight_norm/testmodel2.py
@@ -7,7 +7,7 @@ import scipy
 
 from tensorflow.examples.tutorials.mnist import input_data
 
-#full model
+#one convolutional layer followed by output
 
 train_size = 55000
 test_size = 10000
@@ -63,75 +63,20 @@ with graph.as_default():
 	g_noise = tf.truncated_normal(train_dataset.shape, stddev = 0.15)
 	train_dataset = train_dataset + g_noise
 
-	#first set of 3 3x3conv leaky relu, a 2x2 max pool w/ stride 2, dropout w/ p=0.5
-	#TODO: figure out how to intialize
 	cur_layer = train_dataset
-	g1 = tf.Variable(tf.truncated_normal([1, 1, in_chan, 96], stddev = 0.05))
-	v1 = tf.Variable(tf.truncated_normal([3, 3, in_chan, 96], stddev = 0.05))
+	g1 = tf.Variable(tf.truncated_normal([1, 1, in_chan, 96]))
+	v1 = tf.Variable(tf.truncated_normal([3, 3, in_chan, 96]))
 	b1 = tf.Variable(tf.truncated_normal([96]))
 	cur_layer = conv(cur_layer, tf.multiply(g1,v1/tf.norm(v1, axis = (0,1))))
 	cur_layer = lrelu(cur_layer + b1)
 
-	g2 = tf.Variable(tf.truncated_normal([1, 1, 96, 96], stddev = 0.05))
-	v2 = tf.Variable(tf.truncated_normal([3, 3, 96, 96], stddev = 0.05))
-	b2 = tf.Variable(tf.truncated_normal([96]))
-	cur_layer = conv(cur_layer, tf.multiply(g2,v2/tf.norm(v2, axis = (0,1))))
-	cur_layer = lrelu(cur_layer + b2)
-
-	g3 = tf.Variable(tf.truncated_normal([1, 1, 96, 96], stddev = 0.05))
-	v3 = tf.Variable(tf.truncated_normal([3, 3, 96, 96], stddev = 0.05))
-	b3 = tf.Variable(tf.truncated_normal([96]))
-	cur_layer = conv(cur_layer, tf.multiply(g3,v3/tf.norm(v3, axis = (0,1))))
-	cur_layer = lrelu(cur_layer + b3)
-	cur_layer = tf.nn.dropout(maxpool(cur_layer), keep_prob = 0.5)
- 	''' don't have the power to run this yet
-	#second set of the same
-	g4 = tf.Variable(tf.truncated_normal([1, 1, 96, 192]))
-	v4 = tf.Variable(tf.truncated_normal([3, 3, 96, 192]))
-	b4 = tf.Variable(tf.truncated_normal([192]))
-	cur_layer = conv(cur_layer, tf.multiply(g4,v4/tf.norm(v4, axis = (0,1))))
-	cur_layer = lrelu(cur_layer + b4)
-
-	g5 = tf.Variable(tf.truncated_normal([1, 1, 192, 192]))
-	v5 = tf.Variable(tf.truncated_normal([3, 3, 192, 192]))
-	b5 = tf.Variable(tf.truncated_normal([192]))
-	cur_layer = conv(cur_layer, tf.multiply(g5,v5/tf.norm(v5, axis = (0,1))))
-	cur_layer = lrelu(cur_layer + b5)
-
-	g6 = tf.Variable(tf.truncated_normal([1, 1, 192, 192]))
-	v6 = tf.Variable(tf.truncated_normal([3, 3, 192, 192]))
-	b6 = tf.Variable(tf.truncated_normal([192]))
-	cur_layer = conv(cur_layer, tf.multiply(g6,v6/tf.norm(v6, axis = (0,1))))
-	cur_layer = lrelu(cur_layer + b6)
-	cur_layer = tf.nn.dropout(maxpool(cur_layer), keep_prob = 0.5)
-	'''
-
-	#3x3 conv, 2 sets of 1x1 conv, global average pool, softmax output
-	#don't forget to change 96 to 192 when previous set is added back
-	g7 = tf.Variable(tf.truncated_normal([1, 1, 96, 192], stddev = 0.05))
-	v7 = tf.Variable(tf.truncated_normal([3, 3, 96, 192], stddev = 0.05))
-	b7 = tf.Variable(tf.truncated_normal([192]))
-	cur_layer = conv_no_pad(cur_layer, tf.multiply(g7,v7/tf.norm(v7, axis = (0,1))))
-	cur_layer = lrelu(cur_layer + b7)
-
-	g8 = tf.Variable(tf.truncated_normal([1, 1, 192, 192], stddev = 0.05))
-	v8 = tf.Variable(tf.truncated_normal([1, 1, 192, 192], stddev = 0.05))
-	b8 = tf.Variable(tf.truncated_normal([192]))
-	cur_layer = conv(cur_layer, tf.multiply(g8,v8/tf.norm(v8, axis = (0,1))))
-	cur_layer = lrelu(cur_layer + b8)
-
-	g9 = tf.Variable(tf.truncated_normal([1, 1, 192, 192], stddev = 0.05))
-	v9 = tf.Variable(tf.truncated_normal([1, 1, 192, 192], stddev = 0.05))
-	b9 = tf.Variable(tf.truncated_normal([192]))
-	cur_layer = conv(cur_layer, tf.multiply(g9,v9/tf.norm(v9, axis = (0,1))))
-	cur_layer = lrelu(cur_layer + b9)
 	cur_layer = tf.nn.avg_pool(cur_layer, 
 							   [1, cur_layer.shape[1], cur_layer.shape[2], 1],
 							   [1,1,1,1],
 							   'VALID')
-	cur_layer = tf.reshape(cur_layer, [-1, 192])
-	g10 = tf.Variable(tf.truncated_normal([1,10], stddev = 0.05))
-	v10 = tf.Variable(tf.truncated_normal([192,10], stddev = 0.05))
+	cur_layer = tf.reshape(cur_layer, [-1, 96])
+	g10 = tf.Variable(tf.truncated_normal([1,10]))
+	v10 = tf.Variable(tf.truncated_normal([96,10]))
 	b10 = tf.Variable(tf.truncated_normal([10]))
 	logits = tf.matmul(cur_layer, tf.multiply(g10, v10/tf.norm(v10, axis = 0))) + b10
 	
@@ -142,7 +87,7 @@ with graph.as_default():
 
 	#predictions for valid and test sets, do later
 
-num_steps = 100
+num_steps = 30000
 with tf.Session(graph=graph) as session:
   tf.global_variables_initializer().run()
   print("Initialized")
@@ -160,7 +105,7 @@ with tf.Session(graph=graph) as session:
     feed_dict = {train_dataset : batch_data, train_labels : batch_labels}
     _, l = session.run(
       [optimizer, loss], feed_dict=feed_dict)
-    if (step % 10 == 0):
+    if (step % 500 == 0):
       print("Minibatch loss at step %d: %f" % (step, l))
       #print("Minibatch accuracy: %.1f%%" % accuracy(predictions, batch_labels))
       #print("Validation accuracy: %.1f%%" % accuracy(
