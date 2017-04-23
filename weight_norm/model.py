e8b13ba78f22e9d5c4763ed6dfc0395813962de1
Justin Holmgren <justin.holmgren@gmail.com>
2017-04-22 21:30:01 -0400

    silence tensorflow debug messages?
    unclear if this is good, but at least now we know how; can always
    comment out

--- weight_norm/model.py
+++ weight_norm/model.py
@@ -1,6 +1,9 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+# disable stupid error messages
 import tensorflow as tf
 import numpy as np 
 import scipy
