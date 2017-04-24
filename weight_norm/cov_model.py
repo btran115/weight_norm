28f1159acbd32b353352a6838926535c87d4d017
btran115 <btran115@mit.edu>
2017-04-24 10:52:26 -0400

    gradients

--- weight_norm/cov_model.py
+++ weight_norm/cov_model.py
@@ -21,13 +21,6 @@ num_labels = 10
 #mnist input, 1 channel, 28x28 images
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
-#change images to mean 0, reshape to 28x28
-for im in mnist.train.images:
-	im = im - np.mean(im)
-for im in mnist.validation.images:
-	im = im - np.mean(im)
-for im in mnist.test.images:
-	im = im - np.mean(im)
 train_unflat = np.reshape(mnist.train.images, (train_size, im_size, im_size, in_chan))
 valid_unflat = np.reshape(mnist.validation.images, 
 									 (valid_size, im_size, im_size, in_chan))
@@ -54,28 +47,26 @@ def maxpool(x):
 #build our model
 graph = tf.Graph()
 with graph.as_default():
-	train_dataset = tf.placeholder(tf.float32, 
+	dataset = tf.placeholder(tf.float32, 
 								   shape = (None, im_size,im_size, in_chan))
-	train_labels = tf.placeholder(tf.float32, shape = (None, num_labels))
-	valid_dataset = tf.constant(valid_unflat)
-	test_dataset = tf.constant(test_unflat)
+	labels = tf.placeholder(tf.float32, shape = (None, num_labels))
 
 	#add Gaussian noise
 	g_noise = tf.truncated_normal((1, im_size,im_size, in_chan), stddev = 0.15)
-	cur_layer = train_dataset + g_noise
+	cur_layer = dataset + g_noise
 
 	#g1 = tf.Variable(tf.truncated_normal([1, 1, in_chan, 96]))
-	v1 = tf.Variable(tf.truncated_normal([3, 3, in_chan, 96], stddev = 0.05))
+	v1 = tf.Variable(tf.truncated_normal([3, 3, in_chan, 96], stddev = 0.1))
 	b1 = tf.Variable(tf.truncated_normal([96]))
 	cur_layer = conv(cur_layer, v1)
-	cur_layer = lrelu(cur_layer + b1)
+	cur_layer = maxpool(lrelu(cur_layer + b1))
 
 	#g2 = tf.Variable(tf.truncated_normal([1, 1, 96, 96], stddev = 0.05))
-	v2 = tf.Variable(tf.truncated_normal([3, 3, 96, 96], stddev = 0.05))
+	v2 = tf.Variable(tf.truncated_normal([3, 3, 96, 96], stddev = 0.01))
 	b2 = tf.Variable(tf.truncated_normal([96]))
 	cur_layer = conv(cur_layer, v2)
-	cur_layer = lrelu(cur_layer + b2)
-
+	cur_layer = maxpool(lrelu(cur_layer + b2))
+	'''
 	#g3 = tf.Variable(tf.truncated_normal([1, 1, 96, 96], stddev = 0.05))
 	v3 = tf.Variable(tf.truncated_normal([3, 3, 96, 96], stddev = 0.05))
 	b3 = tf.Variable(tf.truncated_normal([96]))
@@ -100,28 +91,34 @@ with graph.as_default():
 	b6 = tf.Variable(tf.truncated_normal([192]))
 	cur_layer = conv(cur_layer, v6)
 	cur_layer = lrelu(cur_layer + b6)
+	'''
+	#cur_layer = tf.nn.avg_pool(cur_layer, 
+	#						   [1, cur_layer.shape[1], cur_layer.shape[2], 1],
+	#						   [1,1,1,1],
+	#						   'VALID')
+	cur_layer = tf.reshape(cur_layer, [-1, 7*7*96])
 
-	cur_layer = tf.nn.avg_pool(cur_layer, 
-							   [1, cur_layer.shape[1], cur_layer.shape[2], 1],
-							   [1,1,1,1],
-							   'VALID')
-	cur_layer = tf.reshape(cur_layer, [-1, 192])
-	#g10 = tf.Variable(tf.truncated_normal([1,10]))
-	v10 = tf.Variable(tf.truncated_normal([192,10], stddev = 0.05))
-	b10 = tf.Variable(tf.truncated_normal([10]))
-	logits = tf.matmul(cur_layer, v10) + b10
+	vfc = tf.Variable(tf.truncated_normal([7*7*96,1024], stddev = 0.01))
+	bfc = tf.Variable(tf.truncated_normal([1024]))
+	cur_layer = lrelu(tf.matmul(cur_layer, vfc) + bfc)
+
+	#g7 = tf.Variable(tf.truncated_normal([1,10]))
+	v7 = tf.Variable(tf.truncated_normal([1024,10], stddev = 0.01))
+	b7 = tf.Variable(tf.truncated_normal([10]))
+	logits = tf.matmul(cur_layer, v7) + b7
 	
 	#loss and optimizer
-	loss_all = tf.nn.softmax_cross_entropy_with_logits(labels = train_labels, logits = logits)
+	loss_all = tf.nn.softmax_cross_entropy_with_logits(labels = labels, logits = logits)
 	loss = tf.reduce_mean(loss_all)
-	optimizer = tf.train.GradientDescentOptimizer(0.007).minimize(loss)
+	optimizer = tf.train.GradientDescentOptimizer(0.05).minimize(loss)
 
-	gradients = tf.gradients(loss_all, v1)
+	grad = tf.gradients(loss, v7)
 
 	#predictions for valid and test sets, do later
 
-num_steps = 5001
+num_steps = 1501
 all_grads = {}
+all_wts = {}
 with tf.Session(graph=graph) as session:
   tf.global_variables_initializer().run()
   print("Initialized")
@@ -129,30 +126,34 @@ with tf.Session(graph=graph) as session:
     # Pick an offset within the training data, which has been randomized.
     # Note: we could use better randomization across epochs.
     offset = (step * batch_size) % (mnist.train.labels.shape[0] - batch_size)
-    offset = 0
     # Generate a minibatch.
     batch_data = train_unflat[offset:(offset + batch_size)]
     batch_labels = mnist.train.labels[offset:(offset + batch_size)]
     # Prepare a dictionary telling the session where to feed the minibatch.
     # The key of the dictionary is the placeholder node of the graph to be fed,
     # and the value is the numpy array to feed to it.
-    feed_dict = {train_dataset : batch_data, train_labels : batch_labels}
+    feed_dict = {dataset : batch_data, labels : batch_labels}
     _, l = session.run(
       		 [optimizer, loss], feed_dict=feed_dict)
-    if step % 100 == 1:
-    	cur_grads = []
-    	for i in range(batch_size):
-	    	grad = session.run([gradients], feed_dict = {
-	    		train_dataset : batch_data[i:i+1], 
-	    		train_labels : batch_labels[i:i+1]})
-	    	cur_grads.append(np.copy(grad[0]))
-		all_grads[step] = np.array(cur_grads)
-		with open('gradients.pickle', 'w') as f:
-			pickle.dump(all_grads, f)
-    	
     if step % 500 == 0:
       print("Minibatch loss at step %d: %f" % (step, l))
       #print("Minibatch accuracy: %.1f%%" % accuracy(predictions, batch_labels))
       #print("Validation accuracy: %.1f%%" % accuracy(
         #valid_prediction.eval(), valid_labels))
+    if step == 300 or step ==700:
+    	cur_grads = []
+    	cur_wts = []
+    	for i in range(50):
+	    	g7,w7, = session.run([grad,v7], 
+	    		feed_dict = {
+	    		dataset : batch_data[i:i+1], 
+	    		labels : batch_labels[i:i+1]})
+    		cur_grads.append(g7[0])
+    		cur_wts.append(w7)
+		all_grads[step] = np.array(cur_grads)
+		all_wts[step] = np.array(cur_wts)
+		with open('grad7.pickle', 'w') as f:
+			pickle.dump(all_grads, f)
+		with open('wts7.pickle', 'w') as f:
+			pickle.dump(all_wts, f)
  # print("Test accuracy: %.1f%%" % accuracy(test_prediction.eval(), test_labels))
